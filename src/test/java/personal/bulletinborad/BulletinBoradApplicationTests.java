772fe7d8174c09e2eec9e5e8efedb1f95cc3d3bd
OH CHANGMIN <lou0124@naver.com>
2023-06-20 15:46:22 +0900

    feat: 프로젝트 초기 세팅

---
+++ src/test/java/personal/bulletinborad/BulletinBoradApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.bulletinborad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulletinBoradApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
