f66a8d9c3e91cf97fba8e84d1297516b7f35e897
OH CHANGMIN <lou0124@naver.com>
2023-06-22 08:55:32 +0900

    feat: 인증 메일 보내기 구현 및 테스트

---
+++ src/main/java/personal/bulletinborad/service/EmailService.java
@@ -0,0 +1,69 @@
+package personal.bulletinborad.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Random;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender javaMailSender;
+
+    private final String verificationCode = createKey();
+
+    @Value("${spring.mail.username}")
+    private String id;
+
+    public String sendSimpleMessage(String to) throws Exception {
+        MimeMessage message = createMessage(to);
+        try {
+            javaMailSender.send(message);
+        } catch(MailException e){
+            e.printStackTrace();
+            throw new IllegalArgumentException(e);
+        }
+        return verificationCode;
+    }
+
+    private MimeMessage createMessage(String to) throws MessagingException, UnsupportedEncodingException {
+        log.info("보내는 대상 : "+ to);
+        log.info("인증 번호 : " + verificationCode);
+        MimeMessage message = javaMailSender.createMimeMessage();
+
+        message.addRecipients(MimeMessage.RecipientType.TO, to);
+        message.setSubject("게시판 회원가입 인증 코드: ");
+
+        String msg="";
+        msg += "<h1 style=\"font-size: 30px; padding-right: 30px; padding-left: 30px;\">이메일 주소 확인</h1>";
+        msg += "<p style=\"font-size: 17px; padding-right: 30px; padding-left: 30px;\">아래 확인 코드를 회원가입 화면에서 입력해주세요.</p>";
+        msg += "<div style=\"padding-right: 30px; padding-left: 30px; margin: 32px 0 40px;\"><table style=\"border-collapse: collapse; border: 0; background-color: #F4F4F4; height: 70px; table-layout: fixed; word-wrap: break-word; border-radius: 6px;\"><tbody><tr><td style=\"text-align: center; vertical-align: middle; font-size: 30px;\">";
+        msg += verificationCode;
+        msg += "</td></tr></tbody></table></div>";
+
+        message.setText(msg, "utf-8", "html");
+        message.setFrom(new InternetAddress(id,"게시판 관리자"));
+
+        return message;
+    }
+
+    private String createKey() {
+        StringBuffer key = new StringBuffer();
+        Random random = new Random();
+
+        for (int i = 0; i < 6; i++) { // 인증코드 6자리
+            key.append((random.nextInt(10)));
+        }
+        return key.toString();
+    }
+}
---
+++ src/test/java/personal/bulletinborad/service/EmailServiceTest.java
@@ -0,0 +1,19 @@
+package personal.bulletinborad.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class EmailServiceTest {
+
+    @Autowired EmailService emailService;
+
+    @Test
+    void 메일_보내기() throws Exception {
+        String ePw = emailService.sendSimpleMessage("ohchangmin00@gmail.com");
+        System.out.println("ePw = " + ePw);
+    }
+}
