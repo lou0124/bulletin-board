0c0b1ccc7357ee771a2da00bd17b3321cae7d229
OH CHANGMIN <lou0124@naver.com>
2023-06-22 09:38:48 +0900

    style: 불필요 import 제거

--- src/main/java/personal/bulletinborad/config/EmailConfig.java
+++ src/main/java/personal/bulletinborad/config/EmailConfig.java
@@ -1,6 +1,5 @@
 package personal.bulletinborad.config;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
