ae080e802447191daadf90252dc0568a1f85b261
OH CHANGMIN <lou0124@naver.com>
2023-06-22 15:31:28 +0900

    feat: 인증 메일 보내는 인테페이스, 구현체 구현

---
+++ src/main/java/personal/bulletinborad/infrastructure/VerificationMailSender.java
@@ -0,0 +1,10 @@
+package personal.bulletinborad.infrastructure;
+
+import jakarta.mail.MessagingException;
+
+import java.io.UnsupportedEncodingException;
+
+public interface VerificationMailSender {
+
+    String send(String to)  throws MessagingException, UnsupportedEncodingException;
+}
---
+++ src/main/java/personal/bulletinborad/infrastructure/VerificationMailSenderImpl.java
@@ -0,0 +1,71 @@
+package personal.bulletinborad.infrastructure;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Random;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class VerificationMailSenderImpl implements VerificationMailSender {
+
+    private final JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String id;
+
+    @Override
+    public String send(String to) throws MessagingException, UnsupportedEncodingException {
+        String code = createKey();
+        MimeMessage message = createMessage(to, code);
+
+        try {
+            javaMailSender.send(message);
+            log.info("메일 전송 성공: {}", to);
+        } catch(MailException e){
+            log.info("메일 전송 실패: {}", to);
+            e.printStackTrace();
+            throw new IllegalArgumentException(e);
+        }
+        return code;
+    }
+
+    private MimeMessage createMessage(String to, String code) throws MessagingException, UnsupportedEncodingException {
+        log.info("보내는 대상: {}, 인증번호: {}", to, code);
+        MimeMessage message = javaMailSender.createMimeMessage();
+
+        message.addRecipients(MimeMessage.RecipientType.TO, to);
+        message.setSubject("게시판 회원가입 인증 코드: ");
+
+        String msg="";
+        msg += "<h1 style=\"font-size: 30px; padding-right: 30px; padding-left: 30px;\">이메일 주소 확인</h1>";
+        msg += "<p style=\"font-size: 17px; padding-right: 30px; padding-left: 30px;\">아래 확인 코드를 회원가입 화면에서 입력해주세요.</p>";
+        msg += "<div style=\"padding-right: 30px; padding-left: 30px; margin: 32px 0 40px;\"><table style=\"border-collapse: collapse; border: 0; background-color: #F4F4F4; height: 70px; table-layout: fixed; word-wrap: break-word; border-radius: 6px;\"><tbody><tr><td style=\"text-align: center; vertical-align: middle; font-size: 30px;\">";
+        msg += code;
+        msg += "</td></tr></tbody></table></div>";
+
+        message.setText(msg, "utf-8", "html");
+        message.setFrom(new InternetAddress(id,"게시판 관리자"));
+
+        return message;
+    }
+
+    private String createKey() {
+        StringBuffer key = new StringBuffer();
+        Random random = new Random();
+
+        for (int i = 0; i < 6; i++) { // 인증코드 6자리
+            key.append((random.nextInt(10)));
+        }
+        return key.toString();
+    }
+}
