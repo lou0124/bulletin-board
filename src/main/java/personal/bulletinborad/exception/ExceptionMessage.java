bb803bcfdc4fd02490115d082ddfcc3bda17d267
OH CHANGMIN <lou0124@naver.com>
2023-06-22 11:33:14 +0900

    refactor: ExceptionMessage 추상 클래스로 변환

--- src/main/java/personal/bulletinborad/exception/ExceptionMessage.java
+++ src/main/java/personal/bulletinborad/exception/ExceptionMessage.java
@@ -1,6 +1,6 @@
 package personal.bulletinborad.exception;
 
-public class ExceptionMessage {
+public abstract class ExceptionMessage {
     public static final String EXIST_LOGIN_ID = "이미 존재하는 아이디 입니다.";
     public static final String EXIST_EMAIL = "해당 이메일로 아이디가 존재합니다.";
     public static final String EXIST_NICKNAME = "이미 존재하는 닉네임 입니다.";
