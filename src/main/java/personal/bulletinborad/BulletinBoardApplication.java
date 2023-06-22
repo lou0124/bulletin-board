6cd0ef3811b6893038b5126e4676e675a8e425a6
OH CHANGMIN <lou0124@naver.com>
2023-06-22 09:09:57 +0900

    refactor: 네이밍 오타 수정

--- src/main/java/personal/bulletinborad/BulletinBoardApplication.java
+++ src/main/java/personal/bulletinborad/BulletinBoardApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BulletinBoradApplication {
+public class BulletinBoardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BulletinBoradApplication.class, args);
+		SpringApplication.run(BulletinBoardApplication.class, args);
 	}
 
 }
