d05c750513fa45b13a7971ae3118689a278d51f3
OH CHANGMIN <lou0124@naver.com>
2023-06-24 11:01:32 +0900

    refactor: 날짜 변환 리펙토링

--- src/main/java/personal/bulletinborad/controller/dto/PostListDto.java
+++ src/main/java/personal/bulletinborad/controller/dto/PostListDto.java
@@ -17,20 +17,6 @@ public class PostListDto {
         this.id = post.getId();
         this.title = post.getTitle();
         this.writer = post.getMember().getNickname();
-        LocalDateTime date = post.getCreatedDate();
-        String formattedDate = String.format("%d-%02d-%02d %02d:%02d:%02d",
-                date.getYear(), date.getMonthValue(), date.getDayOfMonth(),
-                date.getHour(), date.getMinute(), date.getSecond());
-        this.writtenDate = formattedDate;
-    }
-
-    public PostListDto(Long id, String title, String writer, LocalDateTime date) {
-        this.id = id;
-        this.title = title;
-        this.writer = writer;
-        String formattedDate = String.format("%d-%02d-%02d %02d:%02d:%02d",
-                date.getYear(), date.getMonthValue(), date.getDayOfMonth(),
-                date.getHour(), date.getMinute(), date.getSecond());
-        this.writtenDate = formattedDate;
+        this.writtenDate = post.formattedDate();
     }
 }
--- src/main/java/personal/bulletinborad/entity/Post.java
+++ src/main/java/personal/bulletinborad/entity/Post.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import personal.bulletinborad.entity.mapedsuperclass.BaseTime;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,6 +43,10 @@ public class Post extends BaseTime {
     @OneToMany(mappedBy = "post", orphanRemoval = true)
     private List<File> files = new ArrayList<>();
 
+    public Post(String title) {
+        this.title = title;
+    }
+
     public Post(Member member, Category category, String title, String content, Integer likeCount, List<File> files) {
         this.member = member;
         this.category = category;
@@ -49,5 +54,12 @@ public class Post extends BaseTime {
         this.content = content;
         this.likeCount = likeCount;
         this.files = files;
+    }
+
+    public String formattedDate() {
+        LocalDateTime date = getCreatedDate();
+        return String.format("%d-%02d-%02d %02d:%02d:%02d",
+                date.getYear(), date.getMonthValue(), date.getDayOfMonth(),
+                date.getHour(), date.getMinute(), date.getSecond());
     }
 }
