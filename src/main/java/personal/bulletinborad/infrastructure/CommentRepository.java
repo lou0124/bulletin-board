12009260ff21fc48a1acef46f77021a62ba00a52
OH CHANGMIN <lou0124@naver.com>
2023-06-28 10:44:01 +0900

    feat: findByPost 대댓글 가져오는 쿼리 수정

--- src/main/java/personal/bulletinborad/infrastructure/CommentRepository.java
+++ src/main/java/personal/bulletinborad/infrastructure/CommentRepository.java
@@ -10,7 +10,12 @@ import personal.bulletinborad.entity.Post;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    @Query(value = "select c from Comment c join fetch c.member m left join fetch c.children cc where c.post = :post order by c.createdDate desc ",
+    @Query(value = "select c " +
+            "from Comment c " +
+            "join fetch c.member m " +
+            "left join fetch c.children cc " +
+            "where c.post = :post and c.parent is null " +
+            "order by c.createdDate desc ",
             countQuery = "select c from Comment c where c.post = :post")
     Page<Comment> findByPost(Pageable pageable, @Param("post") Post post);
 }
